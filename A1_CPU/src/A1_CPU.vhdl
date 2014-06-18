d6ea6ba59cf1af056116f3031969575379fa0783
asmcerf <asmcerf@e64f5e90-0800-51d2-c405-62e7623de509>
2014-06-18 13:31:27 +0000

    add first working version, very simple

---
+++ A1_CPU/src/A1_CPU.vhdl
@@ -0,0 +1,425 @@
+LIBRARY ieee;
+LIBRARY work;
+USE ieee.std_logic_1164.all;
+USE work.UTILS.all;
+
+
+package A0 is
+  
+  subtype WORD is STD_LOGIC_VECTOR (31 downto 0);
+  subtype BYTE is STD_LOGIC_VECTOR (7 downto 0);
+  
+  subtype REGT is integer range 0 to 15;
+  
+  type PROGRAM_MEMORY   is array (0 to 16384) of WORD; 
+  type L1_MEMORY        is array (0 to 65535) of WORD; 
+  type REGISTER_MEMORY  is array (0 to 15)    of WORD; 
+ 
+  constant A_NOP   : STD_LOGIC_VECTOR(3 downto 0) := "0000";
+  constant A_MOV   : STD_LOGIC_VECTOR(3 downto 0) := "0001";
+  constant A_ADD   : STD_LOGIC_VECTOR(3 downto 0) := "0010";
+  constant A_SUB   : STD_LOGIC_VECTOR(3 downto 0) := "0011";
+  constant A_ADC   : STD_LOGIC_VECTOR(3 downto 0) := "0100";
+  constant A_CMP   : STD_LOGIC_VECTOR(3 downto 0) := "0101";
+  constant A_AND   : STD_LOGIC_VECTOR(3 downto 0) := "0110";
+  constant A_OR    : STD_LOGIC_VECTOR(3 downto 0) := "0111";
+  constant A_NOT   : STD_LOGIC_VECTOR(3 downto 0) := "1000";
+  constant A_XOR   : STD_LOGIC_VECTOR(3 downto 0) := "1001";
+  constant A_SHL   : STD_LOGIC_VECTOR(3 downto 0) := "1010";
+  constant A_SHR   : STD_LOGIC_VECTOR(3 downto 0) := "1011";
+  constant A_MUL   : STD_LOGIC_VECTOR(3 downto 0) := "1100";
+  constant A_NN1   : STD_LOGIC_VECTOR(3 downto 0) := "1110";
+  constant A_NN2   : STD_LOGIC_VECTOR(3 downto 0) := "1111";
+					  
+  constant M_NOP   : STD_LOGIC_VECTOR(1 downto 0) := "00";
+  constant M_LOAD  : STD_LOGIC_VECTOR(1 downto 0) := "01";
+  constant M_STORE : STD_LOGIC_VECTOR(1 downto 0) := "10";
+  constant M_SWAP  : STD_LOGIC_VECTOR(1 downto 0) := "11";
+  
+  constant C_NOP   : STD_LOGIC_VECTOR(1 downto 0) := "00";
+  constant C_JMP   : STD_LOGIC_VECTOR(1 downto 0) := "01";
+  constant C_HLT   : STD_LOGIC_VECTOR(1 downto 0) := "10";
+  constant C_INT   : STD_LOGIC_VECTOR(1 downto 0) := "11";
+  						
+  
+  type Flags is record		 
+	N    : STD_LOGIC; -- apply not to the rest of flags
+    Z    : STD_LOGIC; -- Zero
+    LT   : STD_LOGIC; -- Less Than
+    LE   : STD_LOGIC; -- Less Equal
+  end record;				  
+  
+  
+  -- unlike common implementation, there is no immediate bit fields in instruction
+  -- Immediate data will be taken as whole next instruction word. Like this:
+  --
+  -- "add R0, R1, 65536" --> 
+  --  add R0, R1, 0 
+  --  65536	  
+  -- ----------------------> so, I-type instructions take 2 clock cycles in simple implementation
+  
+  constant INSTR_ALUI : STD_LOGIC_VECTOR(1 downto 0) := "00";
+  constant INSTR_MEM  : STD_LOGIC_VECTOR(1 downto 0) := "10"; 
+  constant INSTR_CNTR : STD_LOGIC_VECTOR(1 downto 0) := "01";
+  constant INSTR_ALUF : STD_LOGIC_VECTOR(1 downto 0) := "11";
+  
+  -- ALUI: 
+  --
+  -- F     F    F  F  F  FF  F
+  -- 01 00 CODE R0 R1 R2 00  FLAGS	  R-type instruction; r, 1, a, add, R0, R1, R2  
+  -- 11 00 CODE R0 R1 0  00  FLAGS	  I-type instruction; i, 1, a, add, R0, R1, 
+  --                                                      d, {-655362345}   
+ 
+  -- MEM: 
+  --
+  -- F       F    F F  F  FF     F
+  -- 00 10 0 CODE 0 R1 [R2+OFFS] FLAGS  R-type instruction; r, 0, m, sw, 0, R1, R2, 255 // mem(R2+255) := R1;
+  -- 10 10 0 CODE 0 R1 [R2+OFFS] FLAGS  I-type instruction; i, 0, m, sw, 0, R1, R2, 255 // mem(R2+255) := -655362345; 
+  --                                                        d, {-655362345}		        //				    
+  --                                                        r, 1, m, lw R0, 0, R2, 255  // R0 := mem(R2+255);
+                                                       
+  -- CONTROL:	 
+  -- F     F    F F  F  FF    F																						   
+  -- 00 01 CODE 0 R1 0  00    FLAGS  R-type instruction; r, 0, c, jmp, 0, 0, R2 // jmp [R2]
+  -- 10 01 CODE 0 0  0  00    FLAGS  I-type instruction; i, 0, c, jmp    	    // jmp [ADDRESS]
+  --                                                        d, {ADDRESS}
+ 
+  -- FLOAT:
+  -- 01 11 ... same as for ALUI.
+  
+  -- OPCODE = "00" & CODE where "00" is instruction type
+  
+  --
+  type Instruction is record
+	imm     : STD_LOGIC;										  
+	wb      : STD_LOGIC;                     -- write back
+	itype   : STD_LOGIC_VECTOR (1 downto 0); -- instruction type  
+    code    : STD_LOGIC_VECTOR (3 downto 0);	
+    reg0    : REGT;
+    reg1    : REGT;
+    reg2    : REGT;	  
+	memOffs : STD_LOGIC_VECTOR(7 downto 0);  -- used only by memory instructions
+	flags   : Flags; 
+	invalid : STD_LOGIC; 
+  end record;			   
+  
+  constant CMD_NOP : Instruction := (imm => '0', wb=>'0', code => "0000", itype=> "00", reg0 => 0, reg1 => 0, reg2 => 0, memOffs => x"00", flags => (others => '0'), invalid => '0');
+  
+  function ToInstruction(data : WORD) return Instruction; 
+  function ToStdLogic(L: BOOLEAN) return std_logic;
+  
+end A0;
+
+
+package body A0 is
+
+  
+  function ToInstruction(data : WORD) return Instruction is
+    variable cmd : Instruction;
+  begin	
+	cmd.imm      := data(31);						          -- first bit is 'immediate' flag	
+	cmd.wb       := data(30);								  -- second is 'write back' flag
+	cmd.itype	 := data(29 downto 28);						  -- next 2-bit instruction type
+	cmd.code     := data(27 downto 24);				          -- next 4 bit for opcodes	
+	cmd.reg0     := to_uint(data(23 downto 20));			  -- next 4 bits for reg0
+	cmd.reg1     := to_uint(data(19 downto 16));			  -- next 4 bits for reg1
+	cmd.reg2     := to_uint(data(15 downto 12));			  -- next 4 bits for reg2
+    cmd.memOffs	 :=         data(11 downto 4);
+    cmd.invalid  := '0';	
+	cmd.flags.N  := data(3);								  -- last 4 bits for flags
+	cmd.flags.Z  := data(2);
+	cmd.flags.LE := data(1);
+	cmd.flags.LT := data(0);  
+    return cmd;
+  end ToInstruction;
+
+  function ToStdLogic(L: BOOLEAN) return std_logic is
+  begin
+    if L then
+      return('1');
+     else
+       return('0');
+    end if;
+   end ToStdLogic; 
+  
+  
+end A0;
+
+
+-----------------------------------------------------------------------------------------
+--------------------------- main entity -----------------------------------------------
+-----------------------------------------------------------------------------------------
+
+LIBRARY ieee;
+LIBRARY work;
+USE ieee.std_logic_1164.all;
+USE work.DE2_115.all;
+USE work.UTILS.all;
+USE work.A0.all;
+
+
+ENTITY A1_CPU IS
+	PORT(	  
+	  CLOCK_50 : in STD_LOGIC;	
+	  RESET_50 : in	STD_LOGIC
+		);
+END A1_CPU;
+
+ARCHITECTURE RTL OF A1_CPU IS 
+
+  --alias clk : STD_LOGIC is CLOCK_50;	  
+  --alias rst : STD_LOGIC is RESET_50;
+  
+  signal clk  : STD_LOGIC := '0';  
+  signal rst  : STD_LOGIC := '0';
+  
+  signal ip   : integer range 0 to 16384 := 0;	-- instruction pointer
+  
+  signal cmdD : Instruction := CMD_NOP; 
+  signal cmdX : Instruction := CMD_NOP; 
+  signal cmdM : Instruction := CMD_NOP;
+  signal cmdW : Instruction := CMD_NOP;
+ 
+  signal program  : PROGRAM_MEMORY  := 
+  ( 
+   0 => x"C1100000",
+   1 => x"00000003",
+   2 => x"C1200000",
+   3 => x"00000002",
+   4 => x"42012000",
+   5 => x"12000000",
+   6 => x"12000000",
+   7 => x"12000000",
+   others => x"00000000"
+   );  
+  
+  
+  signal memory   : L1_MEMORY       := (others => x"00000000");  -- in real implementation this should be out of chip
+  signal regs     : REGISTER_MEMORY := (others => x"00000000");
+  
+  --signal mem_offs : std_logic_vector(7 downto 0); 
+  
+  signal op1_inputX  : WORD := x"00000000";
+  signal op2_inputX  : WORD := x"00000000"; 
+  
+  signal op1_inputM  : WORD := x"00000000";
+  signal op2_inputM  : WORD := x"00000000";
+  
+  signal resultX     : WORD := x"00000000"; -- after ALU 
+  signal resultM     : WORD := x"00000000"; -- after MEM	
+  
+  signal flags     : Flags;
+  
+BEGIN	
+  
+  -- this is for simulation purposes only 
+  --
+  clock : process
+  begin
+	
+   rst <= '1'; 
+   wait for 10 ns;	 
+   
+   rst <= '0';
+   wait for 10 ns;
+   
+   while true loop	   
+     wait for 10 ns; 
+     clk  <= not clk;
+   end loop;
+   
+  end process clock;
+  
+  
+  main : process(clk,rst)
+  
+    -------------- fetch input ----------------
+    variable cmdF    : Instruction;
+  	variable rawCmdF : WORD;  				   
+	variable stall   : STD_LOGIC := '0';
+	-------------- fetch input ----------------	
+	
+	-------------- mem input ----------------
+	variable address : integer range 0 to 255 := 0;
+	variable memIn   : WORD := x"00000000";    
+	-------------- mem input ----------------
+	
+	-------------- alu input ----------------
+	variable xA : WORD := x"00000000"; 
+	variable xB : WORD := x"00000000";	
+	
+	variable iA : integer := 0;
+    variable iB : integer := 0;
+	-------------- alu input ----------------
+	
+  begin						
+
+   if (rst = '1') then
+
+     ip    <= 0;
+	 cmdD  <= CMD_NOP;
+	   
+   elsif rising_edge(clk) then	   
+	
+     -----------------------------------------------------------------------------------------------------------------
+	 ---------------------------------------  core arch begin --------------------------------------------------------
+	 -----------------------------------------------------------------------------------------------------------------	 
+	
+	 ---- instruction fetch and pipeline basics ----
+	 
+	 rawCmdF := program(ip);
+	 cmdF    := ToInstruction(rawCmdF);
+	
+	 --mem_offs  <= rawCmdF(11 downto 4); -- used only by memory instructions
+	
+	 if stall = '1' or cmdD.imm = '1' then -- push nop to cmdD in next cycle, cause if cmdD is immediate, next instructions is it's data
+	   cmdD <= CMD_NOP;
+	 else
+	   cmdD <= cmdF;
+	 end if;
+	 
+	 -- 0 1 2 3 4
+	 -- F D X M W
+	 --
+	 cmdX <= cmdD; -- from D to X
+	 cmdM <= cmdX; -- from X to M
+	 cmdW <= cmdM; -- from M to W
+	 
+	 ---- instruction fetch and pipeline basics ----   
+	 
+	 
+	 ---- register fetch ----
+	
+	 op1_inputX <= regs(cmdD.reg1);
+	
+	 if cmdD.imm = '1' then		   
+	   op2_inputX <= rawCmdF;
+	 else  
+	   op2_inputX <= regs(cmdD.reg2); -- to_word(cmdF)
+     end if;
+	  
+	 op1_inputM <= op1_inputX;
+	 op2_inputM <= op2_inputX;
+	 
+	 ---- register fetch ----
+	 
+	 
+	 ---- control unit ----
+	 if stall = '1' then
+	   ip <= ip;
+	 else
+	   ip <= ip+1;
+	 end if;	  
+     ---- control unit ----	
+	 
+	 
+	 ---- execution stage ----
+	 
+	 if cmdX.itype = INSTR_ALUI then 				
+		
+	   -------------------------- bypassing ----------------------------	 
+	   if    cmdM.reg0 = cmdX.reg1 and cmdM.wb = '1' then   -- bypass result from X to op1
+	     xA := resultX;
+       elsif cmdW.reg0 = cmdX.reg1 and cmdW.wb = '1' then   -- bypass result from M to op1
+		 xA := resultM; 
+	   else	
+	     xA := op1_inputX;
+	   end if;
+		 
+	   if    cmdM.reg0 = cmdX.reg2 and cmdM.wb = '1' then   -- bypass result from X to op2
+	     xB := resultX;
+       elsif cmdW.reg0 = cmdX.reg2 and cmdW.wb = '1' then   -- bypass result from M to op2
+		 xB := resultM; 
+	   else	
+	     xB := op2_inputX;
+	   end if;	  
+	   -------------------------- bypassing ----------------------------
+	   
+	   -------------------------- alu core -----------------------------
+	   iA := to_sint(xA);
+       iB := to_sint(xB);
+		 
+	   case cmdX.code is	   
+        when A_NOP =>   resultX <= x"00000000";
+        when A_MOV =>   resultX <= xB;
+        when A_ADD =>   resultX <= to_word(iA + iB); -- and write carry flag
+        when A_SUB =>   resultX <= to_word(iA - iB); -- and write carry flag						 
+             
+        when A_CMP =>   flags.Z  <= '0'; -- (iA = iB); 
+	                    flags.N  <= '0';
+                        flags.LE <= ToStdLogic(iA <= iB); 
+                        flags.LT <= ToStdLogic(iA <  iB);
+					    resultX  <= x"00000000";
+                          
+        when A_AND =>   resultX <= xA and xB;
+        when A_OR  =>   resultX <= xA  or xB;
+        when A_NOT =>   resultX <= not xA;
+        when A_XOR =>   resultX <= xA xor xB;
+    
+	    when A_SHL =>   resultX(31 downto 1) <= xB (30 downto 0);
+                        resultX(0) <= '0';
+                          
+        when A_SHR  =>  resultX(31) <= '0';
+                        resultX(30 downto 0) <= xB (31 downto 1);	  
+						  
+        --when A_ADC  =>   resultX <= to_word(iA + carry);	 	
+        when A_MUL  =>   resultX <= to_word(iA * iB);  					  
+        when others =>   resultX <= x"00000000"; report "Error, this command should not pass in to ALU";	   
+      end case; 	 
+	  -------------------------- alu core ----------------------------- 
+
+	  
+	 else
+	   resultX <= x"00000000";	 
+     end if;
+	 
+	 ---- execution stage ----
+	 
+	 
+	 ---- memory stage ----
+	
+	 if cmdM.itype = INSTR_MEM then 		
+		 
+       if cmdW.reg0 = cmdM.reg2 and cmdW.wb = '1' then -- bypass result from M to address
+	     address := to_uint(resultM)  + to_uint(cmdM.memOffs);
+       else	
+	     address := to_uint(op2_inputM) + to_uint(cmdM.memOffs);
+	   end if;
+		 
+	   if cmdW.reg0 = cmdM.reg1 and cmdW.wb = '1' then -- bypass result from M to data
+		 memIn := resultM;
+	   else
+	     memIn := op1_inputM; 
+	   end if;
+	   
+	   case cmdM.code(1 downto 0) is
+	     when M_LOAD  =>   resultM         <= memory(address);   
+	     when M_STORE =>   memory(address) <= memIn; 
+		   				   resultM         <= x"00000000";
+         when others  =>   resultM         <= x"00000000"; 
+       end case;
+	  
+	 else
+	   resultM <= resultX;	 
+     end if;
+	 
+	 ---- memory stage ----
+	 
+	 
+	 ---- write back   ----	  
+	 if cmdW.wb = '1' and not cmdW.invalid = '1' then
+	   regs(cmdW.reg0) <= resultM;
+	 end if;
+	 ---- write back   ----
+	 
+	 -----------------------------------------------------------------------------------------------------------------
+	 ---------------------------------------- core arch end ----------------------------------------------------------
+	 -----------------------------------------------------------------------------------------------------------------
+   
+    end if;
+   
+  end process main;
+ 
+   
+END RTL;
+
---
+++ A1_CPU/src/DE2_115.vhdl
@@ -0,0 +1,176 @@
+LIBRARY ieee;
+use ieee.numeric_bit.all;
+
+package UTILS is
+  function to_uint(bits: BIT_VECTOR) return integer;
+  function to_uint(bits: ieee.std_logic_1164.STD_LOGIC_VECTOR) return integer;
+  function to_sint(bits: BIT_VECTOR) return integer;
+  function to_sint(bits: ieee.std_logic_1164.STD_LOGIC_VECTOR) return integer;
+  function To_BitVector32(number : integer) return BIT_VECTOR;
+  function to_word(number : integer) return ieee.std_logic_1164.STD_LOGIC_VECTOR;
+end UTILS;
+
+
+package body UTILS is
+
+  function to_uint(bits: BIT_VECTOR) return integer is
+  begin
+    return to_integer(unsigned(bits));
+  end to_uint;
+  
+  function to_uint(bits: ieee.std_logic_1164.STD_LOGIC_VECTOR) return integer is
+  begin
+    return IEEE.STD_LOGIC_ARITH.conv_integer(IEEE.STD_LOGIC_ARITH.unsigned(bits));
+  end to_uint;
+  
+  function to_sint(bits: BIT_VECTOR) return integer is
+  begin
+    return to_integer(signed(bits));
+  end to_sint;
+  
+  function to_sint(bits: ieee.std_logic_1164.STD_LOGIC_VECTOR) return integer is
+  begin
+    return IEEE.STD_LOGIC_ARITH.conv_integer(IEEE.STD_LOGIC_ARITH.signed(bits));
+  end to_sint;
+  
+  function To_BitVector32(number : integer) return BIT_VECTOR is
+  begin
+    return BIT_VECTOR(to_unsigned(number,32));
+  end To_BitVector32;
+  
+  -- need signed or unsigned conversion ??
+  --
+  function to_word(number : integer) return ieee.std_logic_1164.STD_LOGIC_VECTOR is
+  begin
+    return ieee.std_logic_1164.STD_LOGIC_VECTOR(ieee.numeric_std.to_signed(number,32));
+  end to_word;
+
+  --impure function InitRamFromFile (Init_Ram_File : in string ) return Init_Memory is
+
+  --FILE ram_file      : text is in Init_Ram_File;
+  --variable line_ram  : line;
+  --variable ram       : Init_Memory;
+
+  --begin
+  --
+  --  for i in Init_Memory'range loop
+  --    readline (ram_file, line_ram);
+  --    read     (line_ram, ram(i));
+  --  end loop;
+  --
+  --  return ram;
+  --  
+  --end function;
+   
+end UTILS;
+
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+package DE2_115 is
+  subtype GREEN_LEDS is BIT_VECTOR (7 downto 0);
+  subtype RED_LEDS   is BIT_VECTOR (17 downto 0);
+  subtype SWITCHES   is BIT_VECTOR (17 downto 0);
+  
+  subtype DIGIT_CODE is STD_LOGIC_VECTOR(0 to 6);
+  
+  function GetDigitCode(digit : in integer range 0 to 15) return DIGIT_CODE;
+  
+end DE2_115;
+
+package body DE2_115 is
+  
+  function GetDigitCode(digit : in integer range 0 to 15) return DIGIT_CODE is
+  begin
+    
+    case digit is 
+      when 0 => return not "1111110";
+      when 1 => return not "0110000";
+      when 2 => return not "1101101";
+      when 3 => return not "1111001";
+      
+      when 4 => return not "0110011";
+      when 5 => return not "1011011";
+      when 6 => return not "1011111";
+      when 7 => return not "1110000";
+      
+      when 8 => return not "1111111";
+      when 9 => return not "1111011";
+      when 10 => return not "1110111";
+      when 11 => return not "0011111";
+      
+      when 12 => return not "1001110";
+      when 13 => return not "0111101";
+      when 14 => return not "1001111";
+      when 15 => return not "1000111";
+           
+      when others => return not "0000000";
+    end case;
+  
+  end GetDigitCode;
+ 
+  
+  
+end DE2_115;
+
+
+LIBRARY work;
+LIBRARY ieee;
+USE work.DE2_115.all;
+USE ieee.std_logic_1164.all;
+USE ieee.STD_LOGIC_ARITH.all;
+use IEEE.numeric_std.all;
+USE work.UTILS.all;
+
+entity Number_Display is
+  PORT(	
+    number : in integer;
+    HEX_0 : out DIGIT_CODE;
+    HEX_1 : out DIGIT_CODE;
+    HEX_2 : out DIGIT_CODE;
+    HEX_3 : out DIGIT_CODE;
+    HEX_4 : out DIGIT_CODE;
+    HEX_5 : out DIGIT_CODE;
+    HEX_6 : out DIGIT_CODE;
+    HEX_7 : out DIGIT_CODE
+  );
+END Number_Display;
+
+-- 
+--
+architecture RTL of Number_Display is
+
+begin
+  
+  p0 : process(number)
+    variable num_b : BIT_VECTOR (31 downto 0);
+    variable H0,H1,H2,H3,H4,H5,H6,H7: BIT_VECTOR (3 downto 0);
+  begin
+  
+    num_b := To_BitVector32(number);
+    
+    H0 := num_b(3 downto 0);
+    H1 := num_b(7 downto 4);
+    H2 := num_b(11 downto 8);
+    H3 := num_b(15 downto 12);
+    H4 := num_b(19 downto 16);
+    H5 := num_b(23 downto 20);
+    H6 := num_b(27 downto 24);
+    H7 := num_b(31 downto 28);
+    
+    HEX_0 <= GetDigitCode(to_uint(H0));
+    HEX_1 <= GetDigitCode(to_uint(H1));
+    HEX_2 <= GetDigitCode(to_uint(H2));
+    HEX_3 <= GetDigitCode(to_uint(H3));
+    HEX_4 <= GetDigitCode(to_uint(H4));
+    HEX_5 <= GetDigitCode(to_uint(H5));
+    HEX_6 <= GetDigitCode(to_uint(H6));
+    HEX_7 <= GetDigitCode(to_uint(H7));
+    
+  end process;
+  
+end RTL;
+
+

cf8e519c287bf73803c6c9fc421739c029d62e83
asmcerf <asmcerf@e64f5e90-0800-51d2-c405-62e7623de509>
2014-06-29 10:59:27 +0000

    wrong bypassing; we must bypass as much as we can to D stage. This is very important!!!

--- trunk/ASM/load_store.asm
+++ trunk/ASM/load_store.asm
@@ -1,7 +1,9 @@
+i, 1, a, mov, R1 
+di, 3
 i, 1, a, mov, R2 
-d, {3}
-r, 0, m, sw,  0,  R1, R2, 255  // mem(R2+255) := R1
-r, 1, m, lw,  R0, 0,  R2, 255  // R0 = mem(R2+255) 
-r, 0, c, hlt, 0, 0, 0
-r, 0, c, hlt, 0, 0, 0
-r, 0, c, hlt, 0, 0, 0
+di, 10
+r, 0, m, sw,  R0, R1, R2, 255  
+r, 1, m, lw,  R0, R0, R2, 255  
+r, 0, c, hlt, R0, R0, R0
+r, 0, c, hlt, R0, R0, R0
+r, 0, c, hlt, R0, R0, R0

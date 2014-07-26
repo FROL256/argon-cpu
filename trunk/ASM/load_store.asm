5b4ac6ccb82427dbdf83c473894db53a4f9e71d1
asmcerf <asmcerf@e64f5e90-0800-51d2-c405-62e7623de509>
2014-07-26 08:19:08 +0000

    fix alu/mem encoding sub/add issue and mov cmd

--- trunk/ASM/load_store.asm
+++ trunk/ASM/load_store.asm
@@ -1,6 +1,6 @@
-i, 1, a, mov, R3 
+i, 1, a, mov, R3, R0 
 di, 3
-i, 1, a, mov, R2     	
+i, 1, a, mov, R2, R0     	
 di, 10
 r, 0, m, sw,  R0, R3, R2, 255  
 r, 1, m, lw,  R0, R0, R2, 255  
--- trunk/ASM/progs/01_bypass_M_to_X.asm
+++ trunk/ASM/progs/01_bypass_M_to_X.asm
@@ -1,6 +1,6 @@
-i, 1, a, mov, R1 
+i, 1, a, mov, R1, R0 
 di, 3
-i, 1, a, mov, R2
+i, 1, a, mov, R2, R0
 di, 2
 r, 0, m, sw,  R0, R1, R2, 255  
 r, 1, m, lw,  R4, R0, R2, 255   

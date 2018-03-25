8b8bc0509342a9d58d3ecd7421c5cfc25c17d469
FROL-HOME\FROL <vova@frolov.pp.ru>
2018-03-25 19:24:05 +0300

    rename program files to get smae numbers in text and binary files

--- ASM/progs/00_add_numbers.asm
+++
@@ -1,9 +0,0 @@
-r, a, xor, R0, R0, R0
-i, a, or,  R1, R0, R0 
-dh, 0x00000003
-i, a, or,  R2, R0, R0
-di, 2
-r, a, add, R0, R1, R2
-r, c, hlt
-r, c, hlt
-r, c, hlt
---
+++ ASM/progs/01_add_numbers.asm
@@ -0,0 +1,9 @@
+r, a, xor, R0, R0, R0
+i, a, or,  R1, R0, R0 
+dh, 0x00000003
+i, a, or,  R2, R0, R0
+di, 2
+r, a, add, R0, R1, R2
+r, c, hlt
+r, c, hlt
+r, c, hlt
